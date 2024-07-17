8985901db6a9b195daea144ec3f2cca80a152c63
Aayush895 <aayush895@gmail.com>
2024-07-17 19:40:16 +0530

    Done with rendering all the tour cards

--- Tours/src/Loading.jsx
+++ Tours/src/Loading.jsx
@@ -1,6 +1,15 @@
+import ReactLoading from 'react-loading'
+import styles from './Loading.module.css'
 const Loading = () => {
   return (
-    <div>Loading</div>
+    <div id={styles.container}>
+      <ReactLoading
+        type={'spinningBubbles'}
+        color={'#03fc4e'}
+        height={200}
+        width={200}
+      />
+    </div>
   )
 }
 export default Loading
--- Tours/src/TourCard.jsx
+++ Tours/src/TourCard.jsx
@@ -1,26 +1,49 @@
+import { useState } from 'react'
 import { PropTypes } from 'prop-types'
 import styles from './TourCard.module.css'
 
 const TourCard = ({ name, info, image, price }) => {
-  return <div id={styles.cardContainer}>
-    <div id={styles.imgContainer}>
-      <img src={image} alt="tour-image" />
-      <p>$ {price}</p>
-    </div>
+  const [readMore, setreadMore] = useState(info.substring(0, 295))
+  const [toggleReadmore, settoggleReadmore] = useState(false)
 
-    <div id={styles.headerContainer}>
-      <h1>{name}</h1>
-      <p>{info}</p>
-    </div>
+  const handleexpandDescription = () => {
+    setreadMore(info)
+    settoggleReadmore(true)
+  }
 
-    <button>Not Interested</button>
-  </div>
+  const handleremoveDescription = () => {
+    setreadMore(info.substring(0, 295))
+    settoggleReadmore(false)
+  }
+
+  return (
+    <div id={styles.cardContainer}>
+      <div id={styles.imgContainer}>
+        <img src={image} alt="tour-image" />
+        <p>$ {price}</p>
+      </div>
+
+      <div id={styles.headerContainer}>
+        <h1>{name}</h1>
+        <p>
+          {readMore}{' '}
+          {toggleReadmore ? (
+            <span onClick={handleremoveDescription}>&nbsp;Read Less</span>
+          ) : (
+            <span onClick={handleexpandDescription}>...Read More</span>
+          )}
+        </p>
+      </div>
+
+      <button>Not Interested</button>
+    </div>
+  )
 }
 
 TourCard.propTypes = {
   name: PropTypes.string,
   info: PropTypes.string,
   image: PropTypes.string,
-  price: PropTypes.string
+  price: PropTypes.string,
 }
 export default TourCard
