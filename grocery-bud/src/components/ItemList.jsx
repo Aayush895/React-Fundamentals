bd262df0ce9a30abe7ec54bbdf4ad27eeda7ba6d
Aayush895 <aayush895@gmail.com>
2024-11-01 12:47:17 +0530

    Done with styling the app

--- grocery-bud/src/components/ItemList.jsx
+++ grocery-bud/src/components/ItemList.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/prop-types */
-
 import { setLocalStorage } from '../util'
 import style from './ItemList.module.css'
 
@@ -12,15 +11,42 @@ function ItemList({ setItems }) {
     setLocalStorage(filteredItem)
   }
 
+  function handleeditItem(id) {
+    const editItem = items.map((item) => {
+      if (item.id == id) {
+        const newItem = { ...item, completed: !item.completed }
+        return newItem
+      }
+
+      return item
+    })
+    setItems(editItem)
+    setLocalStorage(editItem)
+  }
+
   return (
     <div id={style.itemListContainer}>
       {items &&
         items.map((item) => (
-          <div key={item.id}>
+          <div key={item.id} id={style.itemList}>
             <div>
-              <p>{item.name}</p>
-              <button onClick={() => handledeleteItem(item.id)}>Delete</button>
+              <input
+                type="checkbox"
+                name="isCompleted"
+                id=""
+                checked={item.completed}
+                onChange={() => handleeditItem(item.id)}
+              />
+              <p
+                style={{
+                  textTransform: 'capitalize',
+                  textDecoration: item.completed && 'line-through',
+                }}
+              >
+                {item.name}
+              </p>
             </div>
+            <button onClick={() => handledeleteItem(item.id)}>Delete</button>
           </div>
         ))}
     </div>
